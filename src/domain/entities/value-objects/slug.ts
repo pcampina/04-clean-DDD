39ab61093e203ed80c5b410dbc03ca9f47b6ec79
pcampina <pablo@campina.dev>
2025-03-26 23:30:57 +0000

    normalizing slug from title

---
+++ src/domain/entities/value-objects/slug.ts
@@ -0,0 +1,28 @@
+export class Slug {
+  public value: string
+
+  constructor(value: string) {
+    this.value = value
+  }
+
+  /**
+   * Receives an string and normalize it as a slug.
+   * 
+   * @param text {string}
+   * 
+   * @example "Exemple title" => "a-exemple-title"
+   */
+  static createFromText(text: string) {
+    const slugText = text
+      .normalize('NFKD')
+      .toLocaleLowerCase()
+      .trim()
+      .replace(/\s+/g, '-')
+      .replace(/[^\w-]+/g, '')
+      .replace(/_/g, '-')
+      .replace(/--+/g, '-')
+      .replace(/-$/g, '')
+    
+    return new Slug(slugText)
+  }
+}
