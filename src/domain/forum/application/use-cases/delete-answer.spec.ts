43e60c7be8dd50fe45dbf4638c81ba45b00db724
pcampina <pablo@campina.dev>
2025-03-31 08:33:53 +0100

    added delete and edit use cases

---
+++ src/domain/forum/application/use-cases/delete-answer.spec.ts
@@ -0,0 +1,50 @@
+import { InMemoryAnswersRepository } from 'mocks/repositories/in-memory-answers-repository'
+import { DeleteAnswerUseCase } from './delete-answer'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { makeAnswer } from 'mocks/factories/make-answer'
+
+let inMemoryAnswersRepository: InMemoryAnswersRepository
+let sut: DeleteAnswerUseCase
+
+describe('Delete Answer', () => {
+  beforeEach(() => {
+    inMemoryAnswersRepository = new InMemoryAnswersRepository()
+    sut = new DeleteAnswerUseCase(inMemoryAnswersRepository)
+  })
+
+  it('Should be able to delete a answer', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    inMemoryAnswersRepository.create(newAnswer)
+
+    await sut.execute({
+      authorId: 'author-1',
+      answerId: 'answer-1',
+    })
+
+    expect(inMemoryAnswersRepository.items).toHaveLength(0)
+  })
+
+  it('Should not be able to delete a answer from another user', async () => {
+    const newAnswer = makeAnswer(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('answer-1'),
+    )
+
+    inMemoryAnswersRepository.create(newAnswer)
+
+    expect(() => {
+      return sut.execute({
+        authorId: 'author-1',
+        answerId: 'answer-2',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
---
+++ src/domain/forum/application/use-cases/edit-question.spec.ts
@@ -0,0 +1,57 @@
+import { InMemoryQuestionsRepository } from 'mocks/repositories/in-memory-questions-repository'
+import { makeQuestion } from 'mocks/factories/make-question'
+import { EditQuestionUseCase } from './edit-question'
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+
+let inMemoryQuestionsRepository: InMemoryQuestionsRepository
+let sut: EditQuestionUseCase
+
+describe('Edit Question', () => {
+  beforeEach(() => {
+    inMemoryQuestionsRepository = new InMemoryQuestionsRepository()
+    sut = new EditQuestionUseCase(inMemoryQuestionsRepository)
+  })
+
+  it('Should be able to edit a question', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    await sut.execute({
+      questionId: newQuestion.id.toString(),
+      authorId: 'author-1',
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+    })
+
+    expect(inMemoryQuestionsRepository.items[0]).toMatchObject({
+      title: 'Pergunta teste',
+      content: 'Conteúdo teste',
+    })
+  })
+
+  it('Should not be able to edit a question from another user', async () => {
+    const newQuestion = makeQuestion(
+      {
+        authorId: new UniqueEntityID('author-1'),
+      },
+      new UniqueEntityID('question-1'),
+    )
+
+    inMemoryQuestionsRepository.create(newQuestion)
+
+    expect(() => {
+      return sut.execute({
+        questionId: newQuestion.id.toString(),
+        authorId: 'author-2',
+        title: 'Pergunta teste',
+        content: 'Conteúdo teste',
+      })
+    }).rejects.toBeInstanceOf(Error)
+  })
+})
