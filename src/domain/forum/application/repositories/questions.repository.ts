e83ac8dd4d334d47c25eb8a5d35b98f94c9b9a8b
pcampina <pablo@campina.dev>
2025-04-05 13:09:47 +0100

    fetch recent questions

--- src/domain/forum/application/repositories/questions.repository.ts
+++ src/domain/forum/application/repositories/questions.repository.ts
@@ -1,8 +1,10 @@
+import { PaginationParams } from '@/core/repositories/pagination-params'
 import { Question } from '../../enterprise/entities/question'
 
 export interface QuestionsRepository {
   create(question: Question): Promise<void>
   findById(id: string): Promise<Question | null>
+  findManyRecent(params: PaginationParams): Promise<Question[]>
   findBySlug(slug: string): Promise<Question | null>
   delete(question: Question): Promise<void>
   save(question: Question): Promise<void>
