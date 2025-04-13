4c0c19a84d7c940e16da79c5505d8168d984911a
pcampina <pablo@campina.dev>
2025-04-13 15:56:39 +0100

    fetch question comments

---
+++ src/domain/forum/application/use-cases/fetch-question-comments.spec.ts
@@ -0,0 +1,59 @@
+import { UniqueEntityID } from '@/core/entities/unique-entity-id'
+import { InMemoryQuestionCommentsRepository } from 'mocks/repositories/in-memory-question-comments-repository'
+import { FetchQuestionCommentsUseCase } from './fetch-question-comment'
+import { makeQuestionComment } from 'mocks/factories/make-question-comment'
+
+let inMemoryQuestionCommentsRepository: InMemoryQuestionCommentsRepository
+let sut: FetchQuestionCommentsUseCase
+
+describe('Fetch Questions Comments', () => {
+  beforeEach(() => {
+    inMemoryQuestionCommentsRepository =
+      new InMemoryQuestionCommentsRepository()
+    sut = new FetchQuestionCommentsUseCase(inMemoryQuestionCommentsRepository)
+  })
+
+  it('Should be able to fetch question comments', async () => {
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    await inMemoryQuestionCommentsRepository.create(
+      makeQuestionComment({
+        questionId: new UniqueEntityID('question-1'),
+      }),
+    )
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 1,
+    })
+
+    expect(questionComments).toHaveLength(3)
+  })
+
+  it('Should be able to fetch paginated question comments ', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await inMemoryQuestionCommentsRepository.create(
+        makeQuestionComment({
+          questionId: new UniqueEntityID('question-1'),
+        }),
+      )
+    }
+
+    const { questionComments } = await sut.execute({
+      questionId: 'question-1',
+      page: 2,
+    })
+
+    expect(questionComments).toHaveLength(2)
+  })
+})
